68e7dc3b137ce2bc34754c7a6c36a877c8735130
Carlos <cfiguerav@gmail.com>
2022-12-11 15:50:01 -0300

    Agregando

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector('.btn-menu').addEventListener('click', ()=>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase',{delay: 800});
+ScrollReveal().reveal('.cards',{delay:700});
+ScrollReveal().reveal('.whatsapp-c',{delay:600});
+ScrollReveal().reveal('.container-mas',{delay:500});
+ScrollReveal().reveal('.btn-mas2',{delay:400});
+ScrollReveal().reveal('.container-footer',{delay:300});
+
+
+
+
